731d860300b332b02e2da555a8b2da5d2757638e
Jack Kellerk <jjk322@lehigh.edu>
2020-02-08 01:30:19 -0500

    created flutter layout

--- outfit/lib/main.dart
+++ outfit/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
 
 void main() => runApp(MyApp());
 
@@ -16,12 +17,14 @@ class MyApp extends StatelessWidget {
               new Row( // Row for banner
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  new Padding(
-                    padding: const EdgeInsets.only(left: 20.0),
-                    child: new Icon( // The sandwich/dehaze icon
-                      Icons.dehaze,
-                      size: 40.0,
-                      color: Colors.black,
+                  new Container(
+                    child: new Padding(
+                      padding: const EdgeInsets.only(left: 20.0),
+                      child: new Icon( // The sandwich/dehaze icon
+                        Icons.dehaze,
+                        size: 40.0,
+                        color: Colors.black,
+                      ),
                     ),
                   ),
                   new Text( // Our app's name
@@ -34,11 +37,13 @@ class MyApp extends StatelessWidget {
                       decoration: TextDecoration.none,
                     ),
                   ),
-                  new Padding(
-                    padding: const EdgeInsets.only(right: 20.0),
-                    child: new Icon( // Make this a Stack because we want to add a red circle with a number in it
-                      Icons.shopping_cart,
-                      size: 35.0,
+                  new Container(
+                    child: new Padding(
+                      padding: const EdgeInsets.only(right: 20.0),
+                      child: new Icon( // Make this a Stack because we want to add a red circle with a number in it
+                        Icons.shopping_cart,
+                        size: 35.0,
+                      ),
                     ),
                   ),
                 ],
@@ -46,11 +51,170 @@ class MyApp extends StatelessWidget {
               new Divider(
                 thickness: 1.5,
               ),
+              new Padding(
+                padding: const EdgeInsets.only(bottom: 5.0),
+                child: new Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    new ClipOval(
+                        child: new Container(
+                          color: Colors.red,
+                          height: 65,
+                          width: 65,
+                          child: new Center(
+                            child: Image.asset("assets/images/jcpenny.png"),
+                          ),
+                        ),
+                      ),
+                    new ClipOval(
+                        child: new Container(
+                          color: Colors.blue,
+                          height: 65,
+                          width: 65,
+                          child: new Center(
+                            child: Image.asset("assets/images/oldnavy.png")
+                          ),
+                        ),
+                      ),
+                    new ClipOval(
+                        child: new Container(
+                          color: Colors.yellow,
+                          height: 65,
+                          width: 65,
+                          child: new Center(
+                            child: Image.asset("assets/images/gucci.png", height: window.physicalSize.height / 50)
+                          ),
+                        ),
+                      ),
+                    new ClipOval(
+                        child: new Container(
+                          color: Colors.black,
+                          height: 65,
+                          width: 65,
+                          child: new Center(
+                            child: Image.asset("assets/images/uo.png", height: window.physicalSize.height / 50)
+                          ),
+                        ),
+                      ),
+                    new ClipOval(
+                        child: new Container(
+                          color: Colors.grey,
+                          height: 65,
+                          width: 65,
+                          child: new Center(
+                            child: Image.asset("assets/images/lv.png", height: window.physicalSize.height / 50)
+                          ),
+                        ),
+                      ),
+                  ],
+                ),
+              ),
               new Expanded(
                 child: new ListView(
                   children: <Widget>[
-                    new Text("test"),
-                    new Text("test2"),
+                    new Padding(
+                      padding: const EdgeInsets.only(bottom: 10.0),
+                      child: new Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          new ClipRRect(
+                            borderRadius: BorderRadius.circular(20.0),
+                            child: new Container(
+                              color: Colors.grey,
+                              height: window.physicalSize.height / 12,
+                              width: window.physicalSize.width / 6.5,
+                            ),
+                          ),
+
+                          new ClipRRect(
+                            borderRadius: BorderRadius.circular(20.0),
+                            child: new Container(
+                              color: Colors.grey,
+                              height: window.physicalSize.height / 12,
+                              width: window.physicalSize.width / 6.5,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    new Padding(
+                      padding: const EdgeInsets.only(bottom: 10.0),
+                      child: new Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          new ClipRRect(
+                            borderRadius: BorderRadius.circular(20.0),
+                            child: new Container(
+                              color: Colors.grey,
+                              height: window.physicalSize.height / 12,
+                              width: window.physicalSize.width / 6.5,
+                            ),
+                          ),
+
+                          new ClipRRect(
+                            borderRadius: BorderRadius.circular(20.0),
+                            child: new Container(
+                              color: Colors.grey,
+                              height: window.physicalSize.height / 12,
+                              width: window.physicalSize.width / 6.5,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    new Padding(
+                      padding: const EdgeInsets.only(bottom: 10.0),
+                      child: new Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          new ClipRRect(
+                            borderRadius: BorderRadius.circular(20.0),
+                            child: new Container(
+                              color: Colors.grey,
+                              height: window.physicalSize.height / 12,
+                              width: window.physicalSize.width / 6.5,
+                            ),
+                          ),
+
+                          new ClipRRect(
+                            borderRadius: BorderRadius.circular(20.0),
+                            child: new Container(
+                              color: Colors.grey,
+                              height: window.physicalSize.height / 12,
+                              width: window.physicalSize.width / 6.5,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    new Padding(
+                      padding: const EdgeInsets.only(bottom: 10.0),
+                      child: new Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          new ClipRRect(
+                            borderRadius: BorderRadius.circular(20.0),
+                            child: new Container(
+                              color: Colors.grey,
+                              height: window.physicalSize.height / 12,
+                              width: window.physicalSize.width / 6.5,
+                            ),
+                          ),
+
+                          new ClipRRect(
+                            borderRadius: BorderRadius.circular(20.0),
+                            child: new Container(
+                              color: Colors.grey,
+                              height: window.physicalSize.height / 12,
+                              width: window.physicalSize.width / 6.5,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
