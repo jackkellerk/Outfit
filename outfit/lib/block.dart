5583e22e78b59c0542dd729f258c06359343d5d7
Jack Kellerk <jjk322@lehigh.edu>
2020-02-16 19:35:16 -0500

    made some changes to block.dart

--- outfit/lib/block.dart
+++ outfit/lib/block.dart
@@ -5,48 +5,62 @@ import 'package:flutter/material.dart';
 class Block extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    // Write code here for each block to have its own custom clothing item. Probably connect to the server for this.
+    var newClothingItem = ClothingItem();
+
     return Stack(
       children: <Widget>[
         new ClipRRect(
           borderRadius: BorderRadius.circular(20.0),
           child: new Container(
-            color: const Color(0xFFE3E3E3),
+            color: const Color(0xFFF0F0F0),
             height: window.physicalSize.height / 12,
             width: window.physicalSize.width / 6.5,
           ),
         ),
         new Positioned(
-          bottom: 10.0,
-          left: 10.0,
-          // At this point, the child should connect to our servers and randomly present a random clothing item
-          child: new Image.network(
-            "https://i.pinimg.com/originals/20/60/2d/20602d43cc993811e5a6bd1886af4f33.png",
-            height: 20.0,
-          ),
-        ),
-        new Positioned(
-          left: window.physicalSize.width / 52,
-          top: window.physicalSize.height / 96,
-          child: new Image.network(
-            "https://lh3.googleusercontent.com/proxy/Ezp21kDssvbZGtpRsre08a2igaHMBIPcHgmOPAnZwCh21kTGkeGu1nc0wUe1JzYX3WAoyTl2rB36Jj7NX6-XGxn7wMAHJ91f-nJUVp2O2m10TJVuFFmoTO9EDtX-S67y0tQcdEiX5T2I9MNyw16zkaTXfL4j0ucC3BDSBgLdizNfesNAUDE-kJg",
-            height: 100.0,
-          ),
-        ),
-        new Positioned(
-          bottom: 12.0,
+          top: 12.0,
           right: 15.0,
           child: new Text(
-            "Nike Air Force 1",
+            newClothingItem.name,
             style: new TextStyle(
               color: Colors.black,
               fontWeight: FontWeight.normal,
-              fontSize: 10.0,
+              fontStyle: FontStyle.italic,
+              fontSize: 12.0,
               fontFamily: 'Arial',
               decoration: TextDecoration.none,
             ),
           ),
         ),
+        new Positioned(
+          bottom: 7.5,
+          right: 12.0,
+          child: new Icon(
+            newClothingItem.liked ? Icons.favorite : Icons.favorite_border,
+            size: 25.0,
+            color: newClothingItem.liked ? const Color(0xFFFF0000) : const Color(0xFFFFFFFF),
+          ),
+        ),
+        new Positioned(
+          bottom: 5.0,
+          left: 10.0,
+          child: new Container(
+            height: 25.0,
+            width: 50.0,
+            child: new Image.asset("assets/images/nike.png"),
+          ),
+        ),
       ],
     );
   }
+}
+
+// Create a method to generate a lot of these clothing items. Information recieved from the server.
+class ClothingItem {
+  String name = "Air Force 1";
+  String brand = "Nike";
+  bool liked = false;
+  bool onSale = false;
 }
